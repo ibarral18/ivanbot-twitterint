17235bbad2ba8a932167f70ab8ce24a51b76ec97
Ivan Barral <51125955+ibarral18@users.noreply.github.com>
2019-08-06 12:55:21 -0400

    Update main.java
    added a 30 minute timer to the tweets

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,8 +2,19 @@ import java.util.Scanner;
 import java.util.*;
 public class main {
     public static void main(String[] args) {
-      sentenceGen yuh = new sentenceGen();
-      yuh.sendTweet();
-      System.out.println("Ivan Bot says: " + yuh.getSentence());
+        int x = 0;
+        while (x < 100) {
+            sentenceGen yuh = new sentenceGen();
+            yuh.sendTweet();
+            System.out.println("Ivan Bot says: " + yuh.getSentence());
+            try {
+                System.out.println("30 minutes delay");
+                Thread.sleep(1800000); // every 30 minutes
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            x++;
+        }
     }
 }
+
