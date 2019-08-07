c00fa6904916416484bbf9faab0cbf33581ee6c0
Ivan Barral <51125955+ibarral18@users.noreply.github.com>
2019-08-07 10:22:05 -0400

    Update sentenceGen.java
    added more diversity to the bot's lingo by changing a easily adding a few lines of code

--- src/main/java/sentenceGen.java
+++ src/main/java/sentenceGen.java
@@ -7,15 +7,27 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 class sentenceGen {
-    String noun;
-    String adjective;
-    String expletive;
-    String occupation;
+    private String noun;
+    private String adjective;
+    private String expletive;
+    private String occupation;
+    private String postfix;
+    private String food;
+    private String action;
+    private String city;
+    private String insult;
+    private String name;
     public sentenceGen (){
         expletive = "C:\\Users\\ibarral\\Documents\\Ivan Intern Files\\IJ proj\\src\\main\\java\\expletives.txt";
         noun = "C:\\Users\\ibarral\\Documents\\Ivan Intern Files\\IJ proj\\src\\main\\java\\nouns.txt";
         adjective = "C:\\Users\\ibarral\\Documents\\Ivan Intern Files\\IJ proj\\src\\main\\java\\adjectives.txt";
         occupation = "C:\\Users\\ibarral\\Documents\\Ivan Intern Files\\IJ proj\\src\\main\\java\\occupations.txt";
+        postfix = "C:\\Users\\ibarral\\Documents\\Ivan Intern Files\\IJ proj\\src\\main\\java\\postfix.txt";
+        food = "C:\\Users\\ibarral\\Documents\\Ivan Intern Files\\IJ proj\\src\\main\\java\\foods.txt";
+        action = "C:\\Users\\ibarral\\Documents\\Ivan Intern Files\\IJ proj\\src\\main\\java\\actions.txt";
+        city = "C:\\Users\\ibarral\\Documents\\Ivan Intern Files\\IJ proj\\src\\main\\java\\cities.txt";
+        insult = "C:\\Users\\ibarral\\Documents\\Ivan Intern Files\\IJ proj\\src\\main\\java\\insults.txt";
+        name = "C:\\Users\\ibarral\\Documents\\Ivan Intern Files\\IJ proj\\src\\main\\java\\names.txt";
     }
 
     public String getRandomWord(String wordType){
@@ -46,25 +58,36 @@ class sentenceGen {
     public String getSentence() {
         ArrayList<String> sentenceStorage = new ArrayList<>();
         String one = getRandomWord(noun) + " is so " + getRandomWord(adjective);
-        String two = "franky is allergic to " + getRandomWord(noun);
-        String three = getRandomWord(noun) + " is a " + getRandomWord(noun);
+        String two = "@frankybanana is allergic to " + getRandomWord(noun);
+        String three = "@frankybanana is allergic to " + getRandomWord(food);
         String four = "i am baby";
         String five = getRandomWord(noun) + " is so " + getRandomWord(expletive) + " " + getRandomWord(adjective);
         String six = getRandomWord(occupation) + " said fuck work";
         String seven = "i'm baby";
+        String eight = getRandomWord(noun) + " is so " + getRandomWord(adjective) + " " + getRandomWord(postfix);
+        String nine = getRandomWord(noun) + " is so " + getRandomWord(expletive) + " " + getRandomWord(adjective) + " " + getRandomWord(postfix);
+        String ten = "i want " + getRandomWord(food);
+        String eleven = "i want " + getRandomWord(food) + " " + getRandomWord(postfix);
+        String twelve = "i'm just " + getRandomWord(action) + " in " + getRandomWord(city);
+        String thirteen = "@RobiLopezIr is " + getRandomWord(insult);
+        String fourteen = "where is " + getRandomWord(name) + " ?";
+        String fifteen = getRandomWord(name) + " got not chips";
         sentenceStorage.add(one);
         sentenceStorage.add(two);
         sentenceStorage.add(three);
         sentenceStorage.add(four);
         sentenceStorage.add(five);
         sentenceStorage.add(six);
-        sentenceStorage.add(one);
-        sentenceStorage.add(one);
-        sentenceStorage.add(one);
-        sentenceStorage.add(six);
         sentenceStorage.add(six);
         sentenceStorage.add(seven);
-        sentenceStorage.add(seven);
+        sentenceStorage.add(eight);
+        sentenceStorage.add(nine);
+        sentenceStorage.add(ten);
+        sentenceStorage.add(eleven);
+        sentenceStorage.add(twelve);
+        sentenceStorage.add(thirteen);
+        sentenceStorage.add(fourteen);
+        sentenceStorage.add(fifteen);
         Random random = new Random();
         int index = random.nextInt(sentenceStorage.size());
         return sentenceStorage.get(index);
